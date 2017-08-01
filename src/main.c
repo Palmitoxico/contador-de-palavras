6bd36290bb74ebcc6e295640cb0a4088bb54d415
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-08-01 12:12:05 -0300

    It works!
    All tests passed.

--- src/main.c
+++ src/main.c
@@ -6,17 +6,67 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+
+typedef enum {
+	no_separator,
+	master,
+	cond,
+	linefeed,
+} separator_type;
+
+separator_type test_separator(char separator) {
+	const char master_separators[] = {' ', '\t', ';', '-', '!'};
+	const char cond_separators[] = {',', '.'};
+	uint32_t i;
+
+	if (separator == '\n') {
+		return linefeed;
+	}
+
+	for (i = 0; i < sizeof(master_separators); i++) {
+		if (master_separators[i] == separator) {
+			return master;
+		}
+	}
+
+	for (i = 0; i < sizeof(cond_separators); i++) {
+		if (cond_separators[i] == separator) {
+			return cond;
+		}
+	}
+
+	return no_separator;
+}
 
 int main() {
+	char c = 0;
+	separator_type lastct = master, curretct;
+	char buffer[4];
+	int64_t words = 0;
 
-  char c;
+	do {
+		scanf("%c", &c);
+		curretct = test_separator(c);
+		
+		switch (curretct) {
+		case no_separator:
+			if (lastct == master) {
+				words++;
+			} else if (lastct == cond) {
+				if ((c < '0') || (c > '9')) {
+					words++;
+				}
+			}
+			break;
+		
+		default:
+			break;
+		}
+		
+		lastct = curretct;
+	} while (c != '\n');
 
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+	printf("%d\n", words);
+	return 0;
 }
